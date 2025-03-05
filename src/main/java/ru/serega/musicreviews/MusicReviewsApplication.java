6486a5fc58c7202fcf499fcd5945c923c63ab100
razdryzgULTIMATE <alphanomore2222@gmail.com>
2025-03-05 13:13:34 +0400

    init commit

---
+++ src/main/java/ru/serega/musicreviews/MusicReviewsApplication.java
@@ -0,0 +1,13 @@
+package ru.serega.musicreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicReviewsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicReviewsApplication.class, args);
+    }
+
+}
